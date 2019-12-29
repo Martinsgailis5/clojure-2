54d7d339685c109aefa55c8532c61911b8e147c6
BuildTools <unconfigured@null.spigotmc.org>
2019-12-29 11:45:46 +0200

    Commiting conceptual code, doesn't work, but should imo

---
+++ src/lab_5_fp/core.clj
@@ -0,0 +1,21 @@
+(ns lab-5-fp.core)
+(use 'clojure.pprint)
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
+(defn encrypt [x key]
+  (let [text (clojure.string/replace x #"\s" "_")
+        textv (clojure.string/split text #"")
+        colcount (count x)
+        card (make-array String key colcount)
+        cipher (make-array String colcount)
+        rowcounter 0 ] 
+    
+  )
+)
+  
+
+(defn decrypt [cipher key] (print cipher ))
+
---
+++ test/lab_5_fp/core_test.clj
@@ -0,0 +1,7 @@
+(ns lab-5-fp.core-test
+  (:require [clojure.test :refer :all]
+            [lab-5-fp.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
